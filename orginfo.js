195a4eff0d0ce920358f7aacb934edd55e39e044
João Porto <jfportalb@gmail.com>
2016-01-03 20:29:40 -0200

    +Texto *metodo de visualizacao do storyboard

--- orginfo.js
+++ orginfo.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
     $('.scrollspy').scrollSpy();
     $(".button-collapse").sideNav();
     $('.materialboxed').materialbox();
+    $('#storyboardToggle').click(function(){
+        $('#storyboard').toggle();
+    });
 });
