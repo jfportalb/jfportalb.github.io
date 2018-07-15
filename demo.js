33c5ed7f6e8b042e02c376a4982fc6b632a9c594
João Albuquerque <jfportalb@gmail.com>
2018-07-15 23:20:24 +0000

    Mudando graficos de IC

--- demo.js
+++ demo.js
@@ -1,23 +1,57 @@
-demo = {
+var json = {"samples":50000,"simulations":[{"t1":0.00478122,"w1":0.00173981,"x1":0.00304141,"nq1":0.172575,"t2":0.00118793,"w2":0.00093193,"x2":0.000256,"nq2":0.0197888,"jitterE":0.0159979,"jitterV":4.78599e-06},{"t1":0.00491921,"w1":0.00186581,"x1":0.0030534,"nq1":0.185933,"t2":0.00119337,"w2":0.000937369,"x2":0.000256,"nq2":0.0208815,"jitterE":0.0160017,"jitterV":4.84507e-06},{"t1":0.00467973,"w1":0.00165761,"x1":0.00302212,"nq1":0.165047,"t2":0.00119128,"w2":0.00093528,"x2":0.000256,"nq2":0.021066,"jitterE":0.0160001,"jitterV":4.8236e-06},{"t1":0.00483027,"w1":0.00179291,"x1":0.00303736,"nq1":0.180549,"t2":0.00120176,"w2":0.000945757,"x2":0.000256,"nq2":0.0210407,"jitterE":0.016,"jitterV":4.82386e-06},{"t1":0.00458482,"w1":0.0015769,"x1":0.00300792,"nq1":0.155921,"t2":0.00114428,"w2":0.000888276,"x2":0.000256,"nq2":0.019037,"jitterE":0.0159998,"jitterV":4.64102e-06},{"t1":0.00475696,"w1":0.00171462,"x1":0.00304234,"nq1":0.167274,"t2":0.00116802,"w2":0.000912017,"x2":0.000256,"nq2":0.0201279,"jitterE":0.0159985,"jitterV":4.6286e-06},{"t1":0.00482914,"w1":0.00179219,"x1":0.00303695,"nq1":0.177486,"t2":0.00115278,"w2":0.000896777,"x2":0.000256,"nq2":0.0198332,"jitterE":0.0160005,"jitterV":4.56449e-06},{"t1":0.00463952,"w1":0.00165638,"x1":0.00298314,"nq1":0.164117,"t2":0.00115855,"w2":0.000902547,"x2":0.000256,"nq2":0.0195468,"jitterE":0.0159983,"jitterV":4.72014e-06},{"t1":0.00474105,"w1":0.00174231,"x1":0.00299874,"nq1":0.174291,"t2":0.00117017,"w2":0.000914166,"x2":0.000256,"nq2":0.019798,"jitterE":0.016001,"jitterV":4.71654e-06},{"t1":0.00481384,"w1":0.00176781,"x1":0.00304602,"nq1":0.173555,"t2":0.00116406,"w2":0.00090806,"x2":0.000256,"nq2":0.0199139,"jitterE":0.0160044,"jitterV":4.61793e-06},{"t1":0.00476499,"w1":0.00172589,"x1":0.0030391,"nq1":0.169577,"t2":0.00119028,"w2":0.00093428,"x2":0.000256,"nq2":0.0204346,"jitterE":0.0159969,"jitterV":4.87287e-06},{"t1":0.0048452,"w1":0.00180597,"x1":0.00303923,"nq1":0.177539,"t2":0.00117945,"w2":0.000923449,"x2":0.000256,"nq2":0.0211418,"jitterE":0.0160027,"jitterV":4.66865e-06},{"t1":0.00480765,"w1":0.00174005,"x1":0.0030676,"nq1":0.175896,"t2":0.00120919,"w2":0.00095319,"x2":0.000256,"nq2":0.0210723,"jitterE":0.0160039,"jitterV":4.84438e-06},{"t1":0.00486194,"w1":0.00180551,"x1":0.00305643,"nq1":0.178482,"t2":0.00117838,"w2":0.000922379,"x2":0.000256,"nq2":0.0207975,"jitterE":0.0160017,"jitterV":4.75969e-06},{"t1":0.00488515,"w1":0.00185445,"x1":0.0030307,"nq1":0.185035,"t2":0.00118416,"w2":0.000928161,"x2":0.000256,"nq2":0.0208633,"jitterE":0.015999,"jitterV":4.78402e-06},{"t1":0.00474623,"w1":0.00172627,"x1":0.00301996,"nq1":0.174878,"t2":0.00118735,"w2":0.000931352,"x2":0.000256,"nq2":0.0200591,"jitterE":0.015999,"jitterV":4.88692e-06},{"t1":0.00461608,"w1":0.00164105,"x1":0.00297503,"nq1":0.162703,"t2":0.0011554,"w2":0.0008994,"x2":0.000256,"nq2":0.0195498,"jitterE":0.0160016,"jitterV":4.68742e-06},{"t1":0.00465489,"w1":0.00163883,"x1":0.00301606,"nq1":0.161416,"t2":0.00117258,"w2":0.000916581,"x2":0.000256,"nq2":0.0206446,"jitterE":0.0160042,"jitterV":4.78451e-06},{"t1":0.00476169,"w1":0.00176225,"x1":0.00299945,"nq1":0.172953,"t2":0.00116877,"w2":0.000912767,"x2":0.000256,"nq2":0.0212817,"jitterE":0.0159999,"jitterV":4.76846e-06},{"t1":0.00474736,"w1":0.00173481,"x1":0.00301256,"nq1":0.172112,"t2":0.00117128,"w2":0.000915284,"x2":0.000256,"nq2":0.0194212,"jitterE":0.016,"jitterV":4.75111e-06}],"average":{"t1":0.095267,"w1":0.0347414,"x1":0.0605255,"nq1":3.44734,"t2":0.023529,"w2":0.023273,"x2":0.000256,"nq2":0.406299,"jitterE":0.320011,"jitterV":9.49753e-05},"confidenceInterval":{"t1":{"lesser":0.00472818,"mean":0.00476335,"upper":0.00479851,"percentage":1.47654},"w1":{"lesser":0.00170831,"mean":0.00173707,"upper":0.00176584,"percentage":3.31187},"x1":{"lesser":0.0030168,"mean":0.00302628,"upper":0.00303575,"percentage":0.626305},"nq1":{"lesser":0.169346,"mean":0.172367,"upper":0.175388,"percentage":3.50505},"t2":{"lesser":0.00116991,"mean":0.00117645,"upper":0.00118299,"percentage":1.11124},"w2":{"lesser":0.000913914,"mean":0.000920451,"upper":0.000926988,"percentage":1.42031},"nq2":{"lesser":0.0200509,"mean":0.020315,"upper":0.020579,"percentage":2.59976},"jitterMean":{"lesser":0.0159997,"mean":0.0160006,"upper":0.0160014,"percentage":0.0102029},"jitterVariance":{"lesser":4.71362e-06,"mean":4.74877e-06,"upper":4.78391e-06,"percentage":1.48013}}};
+var t1 = [], w1 = [], x1 =  [],nq1 = [], t2 = [], w2 = [], nq2 = [], ej = [], vj = [], l=[];
+var t11 = [], w11 = [], x11 =  [],nq11 = [], t21 = [], w21 = [], nq21 = [], ej1 = [], vj1 = [], l1=[];
+var tlow = 1000000, thigh = 0;
+json["simulations"].forEach(function (s, i) {
+  var t = s["t1"];
+    if (t < tlow) tlow = t; 
+    if (t < thigh) thigh = t; 
+    t11.push(s["t1"]);
+    w11.push(s["w1"]);
+    x11.push(s["x1"]);
+    nq11.push(s["nq1"]);
+    t21.push(s["t2"]);
+    w21.push(s["w2"]);
+    nq21.push(s["nq2"]);
+    ej1.push(s["jitterE"]);
+    vj1.push(s["jitterV"]);
+    l1.push(i)
+});
+t1.push(t11);
+t1.push(w11);
+t1.push(x11);
+nq1.push(nq11);
+t1.push(t21);
+w2.push(w21);
+nq2.push(nq21);
+ej.push(ej1);
+vj.push(vj1);
+l = l1;
+console.log(t11)
+ad = {
+  initChart: function() {
 
-  initDashboardPageCharts: function() {
-
-    if ($('#dailySalesChart').length != 0 || $('#completedTasksChart').length != 0 || $('#websiteViewsChart').length != 0) {
+    if ($('#t1').length != 0 || $('#completedTasksChart').length != 0 || $('#websiteViewsChart').length != 0) {
       /* ----------==========     Daily Sales Chart initialization    ==========---------- */
-
-      dataDailySalesChart = {
-        // labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
-        series: [
-          [12, 17, 7, 17, 23, 18, 38]
-        ]
+      console.log(t1)
+      t1ChartData = {
+        series: t1     
       };
 
       optionsDailySalesChart = {
+        showLine: false,
+        axisX: {
+          labelInterpolationFnc: function(value, index) {
+            return index % 10 === 9 ? index+1 : null;
+          }
+        },
         lineSmooth: Chartist.Interpolation.cardinal({
           tension: 0
         }),
-        low: 0,
-        high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+        // low: tlow*0.9,
+        // high: thigh*1.1,
+        ticks: [0.0045,0.00451,0.00451,0.00452,0.00453,0.00454,0.00455,0.00456,0.00457,0.00458,0.00459,0.00460],
+
         chartPadding: {
           top: 0,
           right: 0,
@@ -26,9 +60,9 @@ demo = {
         },
       }
 
-      var dailySalesChart = new Chartist.Line('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
+      var t1Chart = new Chartist.Line('#t1', t1ChartData, optionsDailySalesChart);
 
-      md.startAnimationForLineChart(dailySalesChart);
+      // md.startAnimationForLineChart(t1Chart);
 
 
 
@@ -100,127 +134,127 @@ demo = {
     }
   },
 
-  initGoogleMaps: function() {
-    var myLatlng = new google.maps.LatLng(40.748817, -73.985428);
-    var mapOptions = {
-      zoom: 13,
-      center: myLatlng,
-      scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
-      styles: [{
-        "featureType": "water",
-        "stylers": [{
-          "saturation": 43
-        }, {
-          "lightness": -11
-        }, {
-          "hue": "#0088ff"
-        }]
-      }, {
-        "featureType": "road",
-        "elementType": "geometry.fill",
-        "stylers": [{
-          "hue": "#ff0000"
-        }, {
-          "saturation": -100
-        }, {
-          "lightness": 99
-        }]
-      }, {
-        "featureType": "road",
-        "elementType": "geometry.stroke",
-        "stylers": [{
-          "color": "#808080"
-        }, {
-          "lightness": 54
-        }]
-      }, {
-        "featureType": "landscape.man_made",
-        "elementType": "geometry.fill",
-        "stylers": [{
-          "color": "#ece2d9"
-        }]
-      }, {
-        "featureType": "poi.park",
-        "elementType": "geometry.fill",
-        "stylers": [{
-          "color": "#ccdca1"
-        }]
-      }, {
-        "featureType": "road",
-        "elementType": "labels.text.fill",
-        "stylers": [{
-          "color": "#767676"
-        }]
-      }, {
-        "featureType": "road",
-        "elementType": "labels.text.stroke",
-        "stylers": [{
-          "color": "#ffffff"
-        }]
-      }, {
-        "featureType": "poi",
-        "stylers": [{
-          "visibility": "off"
-        }]
-      }, {
-        "featureType": "landscape.natural",
-        "elementType": "geometry.fill",
-        "stylers": [{
-          "visibility": "on"
-        }, {
-          "color": "#b8cb93"
-        }]
-      }, {
-        "featureType": "poi.park",
-        "stylers": [{
-          "visibility": "on"
-        }]
-      }, {
-        "featureType": "poi.sports_complex",
-        "stylers": [{
-          "visibility": "on"
-        }]
-      }, {
-        "featureType": "poi.medical",
-        "stylers": [{
-          "visibility": "on"
-        }]
-      }, {
-        "featureType": "poi.business",
-        "stylers": [{
-          "visibility": "simplified"
-        }]
-      }]
+  // initGoogleMaps: function() {
+  //   var myLatlng = new google.maps.LatLng(40.748817, -73.985428);
+  //   var mapOptions = {
+  //     zoom: 13,
+  //     center: myLatlng,
+  //     scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
+  //     styles: [{
+  //       "featureType": "water",
+  //       "stylers": [{
+  //         "saturation": 43
+  //       }, {
+  //         "lightness": -11
+  //       }, {
+  //         "hue": "#0088ff"
+  //       }]
+  //     }, {
+  //       "featureType": "road",
+  //       "elementType": "geometry.fill",
+  //       "stylers": [{
+  //         "hue": "#ff0000"
+  //       }, {
+  //         "saturation": -100
+  //       }, {
+  //         "lightness": 99
+  //       }]
+  //     }, {
+  //       "featureType": "road",
+  //       "elementType": "geometry.stroke",
+  //       "stylers": [{
+  //         "color": "#808080"
+  //       }, {
+  //         "lightness": 54
+  //       }]
+  //     }, {
+  //       "featureType": "landscape.man_made",
+  //       "elementType": "geometry.fill",
+  //       "stylers": [{
+  //         "color": "#ece2d9"
+  //       }]
+  //     }, {
+  //       "featureType": "poi.park",
+  //       "elementType": "geometry.fill",
+  //       "stylers": [{
+  //         "color": "#ccdca1"
+  //       }]
+  //     }, {
+  //       "featureType": "road",
+  //       "elementType": "labels.text.fill",
+  //       "stylers": [{
+  //         "color": "#767676"
+  //       }]
+  //     }, {
+  //       "featureType": "road",
+  //       "elementType": "labels.text.stroke",
+  //       "stylers": [{
+  //         "color": "#ffffff"
+  //       }]
+  //     }, {
+  //       "featureType": "poi",
+  //       "stylers": [{
+  //         "visibility": "off"
+  //       }]
+  //     }, {
+  //       "featureType": "landscape.natural",
+  //       "elementType": "geometry.fill",
+  //       "stylers": [{
+  //         "visibility": "on"
+  //       }, {
+  //         "color": "#b8cb93"
+  //       }]
+  //     }, {
+  //       "featureType": "poi.park",
+  //       "stylers": [{
+  //         "visibility": "on"
+  //       }]
+  //     }, {
+  //       "featureType": "poi.sports_complex",
+  //       "stylers": [{
+  //         "visibility": "on"
+  //       }]
+  //     }, {
+  //       "featureType": "poi.medical",
+  //       "stylers": [{
+  //         "visibility": "on"
+  //       }]
+  //     }, {
+  //       "featureType": "poi.business",
+  //       "stylers": [{
+  //         "visibility": "simplified"
+  //       }]
+  //     }]
 
-    };
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  //   };
+  //   var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-    var marker = new google.maps.Marker({
-      position: myLatlng,
-      title: "Hello World!"
-    });
+  //   var marker = new google.maps.Marker({
+  //     position: myLatlng,
+  //     title: "Hello World!"
+  //   });
 
-    // To add the marker to the map, call setMap();
-    marker.setMap(map);
-  },
+  //   // To add the marker to the map, call setMap();
+  //   marker.setMap(map);
+  // },
 
-  showNotification: function(from, align) {
-    type = ['', 'info', 'danger', 'success', 'warning', 'rose', 'primary'];
+  // showNotification: function(from, align) {
+  //   type = ['', 'info', 'danger', 'success', 'warning', 'rose', 'primary'];
 
-    color = Math.floor((Math.random() * 6) + 1);
+  //   color = Math.floor((Math.random() * 6) + 1);
 
-    $.notify({
-      icon: "add_alert",
-      message: "Welcome to <b>Material Dashboard</b> - a beautiful freebie for every web developer."
+  //   $.notify({
+  //     icon: "add_alert",
+  //     message: "Welcome to <b>Material Dashboard</b> - a beautiful freebie for every web developer."
 
-    }, {
-      type: type[color],
-      timer: 3000,
-      placement: {
-        from: from,
-        align: align
-      }
-    });
-  }
+  //   }, {
+  //     type: type[color],
+  //     timer: 3000,
+  //     placement: {
+  //       from: from,
+  //       align: align
+  //     }
+  //   });
+  // }
 
 }
